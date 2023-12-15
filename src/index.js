27580f2f6b76201aaaba71f6891090399016528d
Lachlan D'Arcy <darcylachlan@hotmail.com.au>
2023-12-15 13:03:05 +1000

    fix(*) changed BrowserRouter to HashRouter because BrowserRouter doesn't work with Github Pages

--- src/index.js
+++ src/index.js
@@ -1,66 +1,61 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {
-    BrowserRouter,
-    Route,
-    Link,
-    Switch
-} from "react-router-dom";
-import { colors, createTheme } from "@themerig/core"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import { colors, createTheme } from '@themerig/core';
 //context
-import { ThemeProvider } from "react-jss";
-import CurrentCountryProvider from "./providers/CurrentCountryProvider"
+import { ThemeProvider } from 'react-jss';
+import CurrentCountryProvider from './providers/CurrentCountryProvider';
 //components
-import CssReset from "./components/CssReset/CssReset"
+import CssReset from './components/CssReset/CssReset';
 //pages
-import IndexPage from "./pages/IndexPage.jsx";
+import IndexPage from './pages/IndexPage.jsx';
 
 const theme = createTheme({
-    palette: {
-        defaults: {
-            background: colors.grey[1],
-            text: {
-                body: colors.grey[8]
-            }
-        }
+  palette: {
+    defaults: {
+      background: colors.grey[1],
+      text: {
+        body: colors.grey[8],
+      },
     },
-    typography: {
-        root: {
-            fontSize: 18,
+  },
+  typography: {
+    root: {
+      fontSize: 18,
+    },
+    h1: {
+      fontSize: 72,
+      fontWeight: '800',
+    },
+    h2: {
+      fontSize: 44,
+      fontWeight: '800',
+    },
+    h3: {
+      fontSize: 24,
+      fontWeight: '800',
+    },
+    body1: {
+      fontSize: 20,
+    },
+  },
+});
 
-        },
-        h1: {
-            fontSize: 72,
-            fontWeight: "800"
-        },
-        h2: {
-            fontSize: 44,
-            fontWeight: "800"            
-        },
-        h3: {
-            fontSize: 24,
-            fontWeight: "800"            
-        },
-        body1: {
-            fontSize: 20,
-        }
-    }
-})
-
-
-function App(){
-    return(
-        <ThemeProvider theme={theme}>
-            <CssReset />
-            <BrowserRouter>
-                <CurrentCountryProvider>
-                    <Route exact path="/">
-                        <IndexPage />
-                    </Route>
-                </CurrentCountryProvider>
-            </BrowserRouter>
-        </ThemeProvider>
-    )
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssReset />
+      {/* Note: Using HashRouter because BrowserRouter doesn't work with Github Pages
+      (https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages) */}
+      <HashRouter>
+        <CurrentCountryProvider>
+          <Route exact path='/'>
+            <IndexPage />
+          </Route>
+        </CurrentCountryProvider>
+      </HashRouter>
+    </ThemeProvider>
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector("#page-content"));
+ReactDOM.render(<App />, document.querySelector('#page-content'));
